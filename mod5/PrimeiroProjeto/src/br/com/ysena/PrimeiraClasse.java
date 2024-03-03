bc33b93387bce257457ae4a73d58fe40717f1fe0
Yuri Sena <102033685+YuriSenaRepos@users.noreply.github.com>
2024-03-03 10:35:42 -0300

    add: tarefa modulo 8, calculo de media

--- mod5/PrimeiroProjeto/src/br/com/ysena/PrimeiraClasse.java
+++ mod5/PrimeiroProjeto/src/br/com/ysena/PrimeiraClasse.java
@@ -1,8 +1,19 @@
 package br.com.ysena;
 
+import java.util.Scanner;
+
 public class PrimeiraClasse {
 
     public static void main(String[] args) {
-        System.out.println("Hello Yuri!");
+        System.out.println("Convertendo Celcius para Fahrenheit");
+        double fah;
+        int celcius;
+        celcius = 23;
+        double CONSTANTE_CE2FA = 1.8;
+        System.out.println("Calculando...");
+        fah = (int) ((celcius * CONSTANTE_CE2FA) + 32);
+        System.out.println(String.format(celcius + "ºC em Fahrenheit é: %.0f ºF", fah));
+
+
     }
 }
---
+++ mod8/src/tarefaCalculoMedia/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int nota1 = 10;
+        int nota2 = 8;
+        int nota3 = 5;
+        int nota4 = 9;
+        int media = (nota1 + nota2 + nota3 + nota4) / 4;
+        System.out.println("A media das notas é: " + media);
+    }
+}
