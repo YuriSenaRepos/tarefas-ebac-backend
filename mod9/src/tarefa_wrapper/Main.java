dccbd39ae2516ae8ba002a3e97de2fedc117ac95
Yuri Sena <102033685+YuriSenaRepos@users.noreply.github.com>
2024-03-05 12:46:45 -0300

    add: tarefa mod 9

---
+++ mod9/src/tarefa_wrapper/Main.java
@@ -0,0 +1,10 @@
+package tarefa_wrapper;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 30;
+        System.out.println(i);
+        Integer wrapperI = Integer.valueOf(i);
+        System.out.println(wrapperI);
+    }
+}
